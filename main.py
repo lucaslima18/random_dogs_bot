fdac2cf825fa11b298a327b464107926788cec5e
lucas amorim de lima araújo <lucas.ala1999@gmail.com>
2020-04-19 13:04:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from telegram.ext import Updater, CommandHandler
+import requests, re
 from send_message import bop
 
 updater = Updater('910018291:AAEXiRAqr-EZnJp-k_Xo1d5al1yhxskG5yw')
@@ -6,3 +7,13 @@ dp = updater.dispatcher
 dp.add_handler(CommandHandler('bop',bop))
 updater.start_polling()
 updater.idle()
+
+
+
+def get_url():
+    '''
+    Get an url of a random image of a dog 
+    '''
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
